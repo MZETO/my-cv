ffd42edf1631208f259fa1ff793833c634342d89
MZETO <dani.martinez@tutanota.com>
2022-07-16 17:51:44 +0200

    components inyected on App.jsx | added some general style on App.css

--- src/App.js
+++ src/App.js
@@ -1,9 +1,42 @@
+import { useState } from "react";
 import './App.css';
+import { Hero, About, Education, Experience, More } from "./components";
+import { CV } from "./CV/CV";
+
+/* Hacemos destructuring para facilitar el acceso a las propiedades de CV.js */
+const { hero, education, experience, languages, habilities } = CV;
 
 function App() {
+  
+  const [showExperience, setShowExperience] = useState(true);
+  
   return (
+
     <div className="App">
-     
+
+      <Hero hero={hero}/>
+
+      <About hero={hero}/>
+
+      <div className="buttons">
+        <button onClick={() => setShowExperience(true)}>
+          EXPERIENCIA
+        </button>
+
+        <button onClick={() => setShowExperience(false)}>
+          EDUCACIÓN
+        </button>
+      </div>
+
+      <div>
+        {showExperience ? (<Experience experience={experience}/>) : (<Education education={education}/>)}
+      </div>
+
+      <More 
+        languages={languages}
+        habilities={habilities}
+      />
+      
     </div>
   );
 }
