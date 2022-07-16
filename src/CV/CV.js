bfc1afae3a553e869b44d0d7786f40ce439b58da
MZETO <dani.martinez@tutanota.com>
2022-07-16 13:21:14 +0200

    INITIAL COMMIT

---
+++ src/CV/CV.js
@@ -0,0 +1,68 @@
+export const CV = {
+    hero: {
+      name: "",
+      adress: "",
+      city: "",
+      email: "",
+      birthDate: "",
+      image: "",
+      gitHub: "",
+      aboutMe: [
+        {
+          info: "",
+        },
+        {
+          info: "",
+        },
+        {
+          info: "",
+        },
+        {
+          info: "",
+        },
+      ],
+    },
+    education: [
+      {
+        name: "",
+        date: "",
+        where: "",
+      },
+      {
+        name: "",
+        date: "",
+        where: "",
+      },
+    ],
+    experience: [
+      {
+        name: "",
+        date: "",
+        where: "",
+        description:
+          "",
+      },
+      {
+        name: "",
+        date: "",
+        where: "",
+        description:
+          "",
+      },
+      {
+        name: "",
+        date: "",
+        where: "",
+        description:
+          "",
+      },
+    ],
+    languages: {
+      language: "",
+      wrlevel: "",
+      splevel: "",
+    },
+    habilities: [
+      "",
+    ],
+  };
