5a90a2ec2b47083517c43bf719751e3ee49671eb
MZETO <dani.martinez@tutanota.com>
2022-07-16 17:53:06 +0200

    FEAT: hero builded

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import "./Hero.css";
+
+const Hero = ({ hero }) => {
+    return (
+        <div className="hero">
+
+            <div className="hero__photo">
+                <img src={hero.image} alt=""></img>
+            </div>
+
+            <div className="hero__title">
+                <h2>
+                    {hero.name}
+                </h2>
+                <h3>
+                    {hero.title} 
+                </h3>
+            </div>
+
+            <div className="hero__info">
+               
+               <p>🏙 {hero.city}</p>
+               <p>🎂 {hero.birthday}</p>
+               <p>
+                   📬 <a href={"mailto:" + hero.email}>dani.martinez@tutanota.com</a>
+               </p>
+           </div>
+
+           <div className="hero__logos">
+               <a href={hero.github}>
+                   <img src="https://img.icons8.com/material-rounded/48/000000/github.png" alt="gitHub"></img>
+               </a>
+               <a href={hero.linkedin}>
+                   <img src="https://img.icons8.com/color/48/000000/linkedin.png" alt="gitHub"></img>
+               </a>
+
+           </div>
+
+        </div>
+    );
+};
+
+export default Hero;
