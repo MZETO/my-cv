a9acfb5b61d496a3c6496ff710edffef919e8a01
MZETO <dani.martinez@tutanota.com>
2022-07-16 17:52:37 +0200

    components indexed on index.js

---
+++ src/components/index.js
@@ -0,0 +1,6 @@
+import Hero from "./Hero/Hero";
+import About from "./About/About";
+import Experience from "./Experience/Experience";
+import Education from "./Education/Education";
+import More from "./More/More";
+export { Hero, About, Experience, Education, More }
