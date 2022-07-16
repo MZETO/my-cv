82f5df3ef1ab8d37547de4981c54751e072805c7
MZETO <dani.martinez@tutanota.com>
2022-07-16 13:48:32 +0200

    CV.js as JSON first version

--- src/CV/CV.js
+++ src/CV/CV.js
@@ -1,68 +1,79 @@
 export const CV = {
     hero: {
-      name: "",
-      adress: "",
-      city: "",
-      email: "",
-      birthDate: "",
-      image: "",
-      gitHub: "",
+      name: "Daniel Martínez Ramírez",
+      adress: "Calle Río Segre 2, 9D",
+      city: "Móstoles - Madrid",
+      email: "dani.martinez@tutanota.com",
+      birthDate: "25/06/1995",
+      image: "https://i.imgur.com/ajZNMlu.jpg",
+      gitHub: "https://github.com/MZETO",
       aboutMe: [
         {
-          info: "",
+          info: "Desarrollador FRONT-END 💻🚀",
         },
         {
-          info: "",
+          info: "Entusiasta de lo videojuegos 🎮 y el cine 📽🎞",
         },
         {
-          info: "",
+          info: "Y... ¡estas son mis armas!",
         },
         {
-          info: "",
+          info: "https://skills.thijs.gg/icons?i=html,css,js,angular,react,git",
         },
       ],
     },
     education: [
       {
-        name: "",
-        date: "",
-        where: "",
+        name: "BOOTCAMP de desarrollador FRONT-END",
+        date: "mar. 2022 - jul. 2022",
+        where: "Upgrade-HUB",
       },
       {
-        name: "",
-        date: "",
-        where: "",
+        name: "F.P. Grado Medio - Sistemas Microinformáticos y redes",
+        date: "2014 - 2017",
+        where: "I.E.S. El Cañaveral",
       },
     ],
     experience: [
       {
-        name: "",
-        date: "",
-        where: "",
+        name: "Técnico de sistemas",
+        date: "nov. 2020 - ACTUAL",
+        where: "Idealista S.A.U.",
         description:
-          "",
+          "Puesto cómo técnico de sistemas, realizando tareas básicas de adm. de sistemas y proporcionando soporte técnico a usuarios finales",
       },
       {
-        name: "",
-        date: "",
-        where: "",
+        name: "Coordinador técnico CAU",
+        date: "ago. 2019 - nov. 2020",
+        where: "SERMICRO S.A.",
         description:
-          "",
+          "Puesto cómo coordinador técnico de un CAU (Centro de Atención a Usuarios) que provee soporte técnico a los usuarios del Ministerio de Industria, Comercio y Turismo (MINCOTUR)",
       },
       {
-        name: "",
-        date: "",
-        where: "",
+        name: "Técnico de soporte",
+        date: "abr. 2018 - sep. 2019",
+        where: "SERMICRO S.A.",
         description:
-          "",
+          "Puesto cómo técnico de soporte microinformático en el CAU del Ministerio de Industria, Comercio y Turismo (MINCOTUR)",
+      },
+      {
+        name: "Becario de TI",
+        date: "abr. 2017 - jun. 2017",
+        where: "QUALO Training & Mobility",
+        description:
+          "Puesto en prácticas cómo becario de TI, desempeñando tareas de soporte a usuarios y cómo aprendiz de desarrollador FRONT-END",
       },
     ],
     languages: {
-      language: "",
-      wrlevel: "",
-      splevel: "",
+      language: "Inglés",
+      wrlevel: "Competencia básica profesional",
+      splevel: "Competencia básica profesional",
     },
     habilities: [
-      "",
+      "Aprendizaje rápido",
+      "Resolución de conflictos",
+      "Adaptabilidad",
+      "Dotes de comunicación interpersonales",
+      "Trabajo en equipo"
     ],
   };
