265d848e182212bf93efa8f43c301975cf454115
MZETO <dani.martinez@tutanota.com>
2022-07-17 22:51:55 +0200

    FEAT: About recomposed

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,11 +1,15 @@
 import React from "react";
 import "./About.css";
 
-const About = ({ hero }) => {
+const About = ({ aboutme }) => {
     return (
         <div className="about">
-            <h1>{"<"} HARD-SKILLS {"/>"}</h1>
-            <div className="about__info">            
+            <h1>{"<"} SOBRE MÍ {"/>"}</h1>
+            <div className="about__info">
+                <p>{aboutme.cuote1}</p>
+                <p>{aboutme.cuote2}</p>
+                <p>{aboutme.cuote3}</p>
+                <h2>{aboutme.cuote4}</h2>            
                 <img src="https://skills.thijs.gg/icons?i=html,css,js,angular,react,git" alt="techs"></img>
             </div>
         </div>
